c9343f5f8df8c863cdda73c0a81dcba2a1a36874
Sudheesh T D <sudheeshtd01@gmail.com>
2024-05-23 22:51:54 -0500

    only function

---
+++ Week 1/BreakingTheRecords.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+
