f6f8fe8de7b204c0aeabe031277dc28fc4866b1c
Sudheesh T D <sudheeshtd01@gmail.com>
2024-05-25 01:20:22 -0500

    commit

--- Week 1/CamelCase4.py
+++ Week 1/CamelCase4.py
@@ -1,33 +1,75 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
 import sys
-input_text = sys.stdin.read() 
+input_text = sys.stdin.read()
+
+
 lines = input_text.split('\n')
 
 for line in lines:
-
     words = line.split(';')
     if "S" in words[0]:
         spacedWords = ""
-        for cap in words[2]:
-
+        for cap in words[2]:            
             if (not cap.isalpha()):
-                spacedWords
+                continue
             elif(cap.isupper() == True):
-                spacedWords = spacedWords+" "+cap
+                if(not words[2][0].isupper):
+                    continue
+                else:
+                    spacedWords = spacedWords+" "+cap
             else:
                 spacedWords += cap   
 
 
-        if words[1] is 'C':
-            for cap in words[2]:
+    # elif 'C' in words[1]:
+    #         for cap in words[2]:
+    #             continue
+                    
+    resulS = spacedWords.lower().strip()
+    print(resulS)
 
-                
-        resul = spacedWords.lower()
-        print(resul)
-                
-            
+
+
         
-    elif "C" in words[0]:
-        newWord = line
-        print(words[2])
+    if "C" in words[0]:
+        count = len(words[2])
+        if "V" in words[1]:
+            for num in range(1,count-1):
+                print(words[num] + " ")
+    
+
+
+
+
+        elif("M" in words[1]):
+            continue
+
+
+        
+        
+        else:
+            continue
+
+
+
+
+
+        
+    
+    
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
