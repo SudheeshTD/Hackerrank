c7bb6d05d28af586487f93420dc1ce486abcaad5
Sudheesh T D <sudheeshtd01@gmail.com>
2024-07-29 15:52:25 -0500

    pangrams

---
+++ Week 2/pangrams.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    d = {}
+    s = s.lower().replace(' ', '')
+    for i in s:
+        if i not in d:
+            d[i] = 0
+    if len(d) == 26:
+        return 'pangram'
+    else:
+        return 'not pangram'
+        
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
